74d0adceaa17b59249a8aa5debfdee40b6e1c10a
UgwuObed <vjazzyvictor@gmail.com>
2024-10-29 20:19:10 +0100

    added lesson datastructure

--- dataStructure.js
+++ dataStructure.js
@@ -168,13 +168,6 @@ console.log(userSessions.get(user2));
 
 
 
-  
-
-
-
-
-
-
 
 
 
---
+++ lesson-datastructure.js
@@ -0,0 +1,16 @@
+let intArray = new Int32Array(5);
+intArray[0] = 100;
+console.log(intArray[0])
+
+// {
+//     "name": "Tomi", 
+//     "age": 24, 
+//     "city": "Lagos" 
+// }
+
+// converts objects to JSON
+JSON.stringify()
+
+// converts JSON to objects
+JSON.parse()
+
